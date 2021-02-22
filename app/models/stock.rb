fa8bc13bb46b4ecb683260046a58876678f611a2
Justin Lee <jylee@Justins-MacBook-Pro.local>
2021-02-22 01:18:26 -0800

    Made first API connection

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,20 @@
+require 'rubygems'
+
+
 class Stock < ApplicationRecord
+    # client = IEX::Api::Client.new(
+    #  publishable_token: 'pk_d6ac2345a70741dd9e38b135c73547dd',
+    #  endpoint: 'https://sandbox.iexapis.com/stable'
+    # )
+
+    def stock_price
+            client = IEX::Api::Client.new(
+     publishable_token: 'Tpk_8ecd293d171f48c793e2a1435a284f9c',
+     endpoint: 'https://sandbox.iexapis.com/stable'
+    )
+
+        quote = client.quote('AAPL')
+        puts quote.latest_price;
+    end
+
 end
