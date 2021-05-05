1fbed8c34ca6f2b4408bc908142003f914454f91
Justin Lee <jylee@Justins-MacBook-Pro.local>
2021-05-05 13:18:23 -0700

    Heroku Cors issue

--- config/initializers/cors.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'http://localhost:5000'
-
-    resource '*',
-      headers: :any,
-      methods: [:get]
-  end
-end
