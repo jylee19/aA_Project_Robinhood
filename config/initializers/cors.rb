4aafcd36a68ca6677ee29963e5c1c0be4fb88b0d
Justin Lee <jylee@Justins-MacBook-Pro.local>
2021-05-05 12:08:48 -0700

    More Cors bug fixes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://robingoods.herokuapp.com/'
+    origins 'https://robingoods.herokuapp.com'
 
     resource '*',
       headers: :any,
